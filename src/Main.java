6586e2d73178219863d0d17dc9305d6b8a967429
LaoBing_Fung <FLB_B401@163.com>
2019-02-21 16:17:05 +0800

    create teacher class contains introduce method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import main.java.fung.model.Klass;
 import main.java.fung.model.Person;
 import main.java.fung.model.Student;
+import main.java.fung.model.Teacher;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class Main {
         Student tom = new Student(20190101, "Tom", 21, klass2);
         klass1.assignLeader(tom);
         System.out.println(tom.introduce());
+        Teacher jason = new Teacher(2019001, "Jason", 40, new Klass[]{});
+        System.out.println(jason.introduce());
     }
 }
---
+++ src/main/java/fung/model/Teacher.java
@@ -0,0 +1,27 @@
+package main.java.fung.model;
+
+public class Teacher extends Person {
+    private Klass[] klasses;
+
+    public Teacher(int id, String name, int age, Klass[] klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    @Override
+    public String introduce() {
+        String introStr = null;
+        if (this.klasses.length == 0) {
+            introStr = "I teach No Class.";
+        } else {
+            introStr = "I teach Class";
+            for (int i = 0; i < klasses.length; i++) {
+                introStr += klasses[i].getNumber();
+                if (i < klasses.length - 1) {
+                    introStr += ", ";
+                }
+            }
+        }
+        return super.introduce() + "I am a Teacher." + introStr;
+    }
+}
